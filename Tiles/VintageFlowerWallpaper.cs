c46b011869f232db496fd2287783caa945885151
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-06 20:54:51 +0000

    Update VintageFlowerWallpaper.cs

--- Tiles/VintageFlowerWallpaper.cs
+++ Tiles/VintageFlowerWallpaper.cs
@@ -8,7 +8,7 @@ namespace terrarchitect.Walls
 	{
 		public override void SetDefaults() {
 			Main.wallHouse[Type] = true;
-			drop = ModContent.ItemType<Items.VintageFlowerWallpaper>();
+			drop = ModContent.ItemType<Items.VintageFlowerWallpaperItem>();
 			AddMapEntry(new Color(150, 150, 150));
 		}
 	}
