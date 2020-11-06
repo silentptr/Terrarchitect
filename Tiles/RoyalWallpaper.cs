aed88feb51fc5dc97640d1d862b662a453ea0cd4
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-06 20:54:31 +0000

    Update RoyalWallpaper.cs

--- Tiles/RoyalWallpaper.cs
+++ Tiles/RoyalWallpaper.cs
@@ -8,7 +8,7 @@ namespace terrarchitect.Walls
 	{
 		public override void SetDefaults() {
 			Main.wallHouse[Type] = true;
-			drop = ModContent.ItemType<Items.RoyalWallpaper>();
+			drop = ModContent.ItemType<Items.RoyalWallpaperItem>();
 			AddMapEntry(new Color(150, 150, 150));
 		}
 }
