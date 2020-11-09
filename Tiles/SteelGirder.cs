c2663d5a403c53eabe1cd1ac067b6864c380f35e
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-09 20:13:09 +0000

    Add files via upload

---
+++ Tiles/SteelGirder.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Terrarchitect.Tiles
+{
+	public class SteelGirder : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;
+			drop = ModContent.ItemType<Items.SteelGirderItem>();
+			AddMapEntry(new Color(150, 150, 150));
+		}
+	}
+}
