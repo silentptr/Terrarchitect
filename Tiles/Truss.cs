c6e5157f48e1e4e6a48b57feadb5669390297846
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-09 20:29:08 +0000

    Add files via upload

---
+++ Tiles/Truss.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Terrarchitect.Tiles
+{
+	public class Truss : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;
+			drop = ModContent.ItemType<Items.TrussItem>();
+			AddMapEntry(new Color(150, 150, 150));
+		}
+	}
+}
