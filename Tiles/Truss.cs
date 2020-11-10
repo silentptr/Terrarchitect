6edb0297184799b1c814e1b899400571e3775887
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-10 23:22:21 +0000

    No longer merges with dirt

--- Tiles/Truss.cs
+++ Tiles/Truss.cs
@@ -8,7 +8,7 @@ namespace Terrarchitect.Tiles
 	{
 		public override void SetDefaults() {
 			Main.tileSolid[Type] = true;
-			Main.tileMergeDirt[Type] = true;
+			Main.tileMergeDirt[Type] = false;
 			Main.tileBlockLight[Type] = false;
 			Main.tileLighted[Type] = true;
 			drop = ModContent.ItemType<Items.TrussItem>();
