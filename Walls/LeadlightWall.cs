7d0b6d11c91f2684ebd56692b19f3f9d5787b96f
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-11 18:02:16 +0000

    Add files via upload

---
+++ Walls/LeadlightWall.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Terrarchitect.Walls
+{
+	public class LeadlightWall : ModWall
+	{
+		public override void SetDefaults() {
+			Main.wallHouse[Type] = true;
+			drop = ModContent.ItemType<Items.LeadlightWallItem>();
+			//AddMapEntry(new Color(150, 150, 150));
+		}
+}
+	}
