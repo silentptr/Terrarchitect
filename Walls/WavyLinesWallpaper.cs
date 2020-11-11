75a9b785d7bb6919254ab68457d6942eb74248d7
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-11 18:18:40 +0000

    Map colour now matches the wallpaper

--- Walls/WavyLinesWallpaper.cs
+++ Walls/WavyLinesWallpaper.cs
@@ -9,7 +9,7 @@ namespace terrarchitect.Walls
 		public override void SetDefaults() {
 			Main.wallHouse[Type] = true;
 			drop = ModContent.ItemType<Items.WavyLinesWallpaperItem>();
-			AddMapEntry(new Color(150, 150, 150));
+			AddMapEntry(new Color(221, 148, 133));
 		}
 	}
 }
