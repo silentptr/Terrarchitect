b63e13905dfc2ccda2c942dd51cbf75e6fcb5b93
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-11 18:16:47 +0000

    Map colour now matches the wallpaper

--- Walls/PolkaDotsWallpaper.cs
+++ Walls/PolkaDotsWallpaper.cs
@@ -9,7 +9,7 @@ namespace terrarchitect.Walls
 		public override void SetDefaults() {
 			Main.wallHouse[Type] = true;
 			drop = ModContent.ItemType<Items.PolkaDotsWallpaperItem>();
-			AddMapEntry(new Color(150, 150, 150));
+			AddMapEntry(new Color(224, 154, 140));
 		}
 	}
 }
