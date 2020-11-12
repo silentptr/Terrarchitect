d96963197214c37ac574991c9cf82f59dfc0f1e0
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-12 16:50:43 +0100

    Add files via upload

---
+++ Tiles/ThinPipe.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Terrarchitect.Tiles
+{
+	public class ThinPipe : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = false;
+			Main.tileLighted[Type] = true;
+			drop = ModContent.ItemType<Items.ThinPipeItem>();
+			AddMapEntry(new Color(150, 150, 150));
+		}
+	}
+}
