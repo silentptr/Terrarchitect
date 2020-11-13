633ae25bc019a85c56a5508ae77edcf9bbd433a1
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-13 23:40:45 +0000

    Update Terrarchitect.cs

--- Terrarchitect.cs
+++ Terrarchitect.cs
@@ -8,18 +8,28 @@ namespace Terrarchitect
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(this);
-            recipe.AddIngredient(ItemID.Wood, 3);
-            recipe.anyWood = true;
+            recipe.AddRecipeGroup("Squirrels");
             recipe.AddTile(ModContent.TileType<Tiles.TerrarchitectCatalogue>());
-            recipe.SetResult(ItemID.Book);
+            recipe.SetResult(ItemID.Leather);
             recipe.AddRecipe();
-
-            ModRecipe recipe2 = new ModRecipe(this);
-            recipe2.AddIngredient(ItemID.IronBar);
-            recipe2.anyIronBar = true;
+			
+			ModRecipe recipe1 = new ModRecipe(this);
+            recipe1.AddIngredient(ItemID.Bunny);
+            recipe1.AddTile(ModContent.TileType<Tiles.TerrarchitectCatalogue>());
+            recipe1.SetResult(ItemID.Leather);
+            recipe1.AddRecipe();	
+			
+			ModRecipe recipe2 = new ModRecipe(this);
+            recipe2.AddRecipeGroup("IronBar");
             recipe2.AddTile(ModContent.TileType<Tiles.TerrarchitectCatalogue>());
             recipe2.SetResult(ItemID.Spike, 10);
             recipe2.AddRecipe();
+			
+			ModRecipe recipe3 = new ModRecipe(this);
+            recipe3.AddIngredient(ModContent.GetInstance<Items.Paper>());
+            recipe3.AddTile(ModContent.TileType<Tiles.TerrarchitectCatalogue>());
+            recipe3.SetResult(ItemID.Book);
+            recipe3.AddRecipe();
         }
     }
 }
