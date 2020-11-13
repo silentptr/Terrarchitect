44be4aaad13bbe14e6c8db52308fa453fac98d06
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-13 21:45:11 +0000

    Add files via upload

---
+++ Items/TerrarchitectCatalogueItem.cs
@@ -0,0 +1,34 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Items
+{
+    public class TerrarchitectCatalogueItem : ModItem
+    {
+        public override void SetStaticDefaults(){DisplayName.SetDefault("Terrarchitect Catalogue");}
+		public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 32;
+            item.maxStack = 99;
+            item.value = 5;
+			item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+			item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.TerrarchitectCatalogue>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddRecipeGroup("Wood", 10);
+			recipe.AddTile(TileID.Sawmill);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
