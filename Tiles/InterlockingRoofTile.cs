292a402a305f396d3538ecc52a28d38c80ad5a6b
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-13 19:49:41 +0100

    Update InterlockingRoofTile.cs

--- Tiles/InterlockingRoofTile.cs
+++ Tiles/InterlockingRoofTile.cs
@@ -12,6 +12,7 @@ namespace Terrarchitect.Tiles
             Main.tileSolid[Type] = true;
             Main.tileLavaDeath[Type] = true;
             Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
             TileObjectData.newTile.CoordinateHeights = new[] { 16 };
             drop = ItemType<Items.InterlockingRoofTileItem>();
