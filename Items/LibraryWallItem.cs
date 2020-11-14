f06009b65339c18290280dd5e01b2bedd4f1c468
BuildTools <unconfigured@null.spigotmc.org>
2020-11-15 00:03:29 +1000

    added library wall

---
+++ Items/LibraryWallItem.cs
@@ -0,0 +1,41 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Items
+{
+    public class LibraryWallItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Library Wall");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 32;
+            item.maxStack = 99;
+            item.value = 5;
+
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.createWall = WallType<Walls.LibraryWall>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood);
+            recipe.AddIngredient(ItemID.Book);
+            recipe.SetResult(this, 4);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Walls/LibraryWall.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Walls
+{
+    public class LibraryWall : ModWall
+    {
+        public override void SetDefaults()
+        {
+            Main.wallHouse[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            drop = ItemType<Items.LibraryWallItem>();
+        }
+    }
+}
