d959e4bcb78743abeef23e6bdedc0c6d87d5e351
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 01:31:38 +1000

    added patchwork fabric

---
+++ Items/PatchworkFabricItem.cs
@@ -0,0 +1,40 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Items
+{
+    public class PatchworkFabricItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Patchwork Fabric");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 32;
+            item.maxStack = 999;
+            item.value = 5;
+
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.createWall = WallType<Walls.PatchworkFabric>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Silk);
+            recipe.SetResult(this, 1);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Walls/PatchworkFabric.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Walls
+{
+    public class PatchworkFabric : ModWall
+    {
+        public override void SetDefaults()
+        {
+            Main.wallHouse[Type] = true;
+            Main.wallLight[Type] = false;
+            drop = ItemType<Items.PatchworkFabricItem>();
+        }
+    }
+}
