1f79f3e55f20b0b948c550929287f59c3bee66ae
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-23 22:13:51 +0100

    Update Gong.cs

--- Tiles/Gong.cs
+++ Tiles/Gong.cs
@@ -15,10 +15,8 @@ namespace Terrarchitect.Tiles
             Main.tileLavaDeath[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
             TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
-            TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
             TileObjectData.newTile.StyleHorizontal = true;
             TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
-            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceLeft;
             TileObjectData.addAlternate(1);
             TileObjectData.addTile(Type);
             disableSmartCursor = true;
