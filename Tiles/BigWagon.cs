772dcd5202e257bedc1c6fe3cf35f1f5a29bf868
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-23 19:14:55 +0100

    Update BigWagon.cs

--- Tiles/BigWagon.cs
+++ Tiles/BigWagon.cs
@@ -14,7 +14,6 @@ namespace Terrarchitect.Tiles
             Main.tileNoAttach[Type] = true;
             Main.tileLavaDeath[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
-            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
             TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
             TileObjectData.newTile.StyleHorizontal = true;
             TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
