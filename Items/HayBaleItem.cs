96ea1cd89de9d5d4f28844f58385a4797707f717
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-23 22:24:33 +0100

    Update HayBaleItem.cs

--- Items/HayBaleItem.cs
+++ Items/HayBaleItem.cs
@@ -31,7 +31,8 @@ namespace Terrarchitect.Items
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.Hay, 10);
+            recipe.AddIngredient(ItemID.Hay, 8);
+            recipe.AddIngredient(ItemID.Rope, 2);
             recipe.SetResult(this, 1);
             recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
             recipe.AddRecipe();
