014a2a1a483e7487f63a504a726e51cb023b4277
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-24 10:43:28 +0100

    Update ParasolItem.cs

--- Items/ParasolItem.cs
+++ Items/ParasolItem.cs
@@ -27,5 +27,15 @@ namespace Terrarchitect.Items
             item.consumable = true;
             item.createTile = TileType<Tiles.Parasol>();
         }
+          public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 3);
+            recipe.anyWood = true;
+            recipe.AddIngredient(ItemID.Silk, 4);
+            recipe.SetResult(this, 1);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+        }
     }
 }
