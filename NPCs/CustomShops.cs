89c0256b38242ca2509b7d3d772f95de15565766
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-24 11:42:00 +0100

    Update CustomShops.cs

--- NPCs/CustomShops.cs
+++ NPCs/CustomShops.cs
@@ -32,10 +32,6 @@ namespace Terrarchitect.NPCs
                     shop.item[nextSlot].shopCustomPrice = Item.buyPrice(0, 0, 10);
                     nextSlot++;
 
-                    shop.item[nextSlot].SetDefaults(ItemType<Items.ParchmentItem>());
-                    shop.item[nextSlot].shopCustomPrice = Item.buyPrice(0, 0, 10);
-                    nextSlot++;
-
                     shop.item[nextSlot].SetDefaults(ItemType<Items.PileOfScrollsItem>());
                     shop.item[nextSlot].shopCustomPrice = Item.buyPrice(0, 0, 5);
                     nextSlot++;
