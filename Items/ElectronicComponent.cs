9df661b2289c681e5e1547f60915bfc1849a8e3e
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-26 21:30:00 +0000

    Add files via upload

---
+++ Items/ElectronicComponent.cs
@@ -0,0 +1,33 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Items 
+{
+    public class ElectronicComponent : ModItem 
+    {
+		public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 24;
+            item.maxStack = 99;
+		}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.SandBlock,1);
+			recipe.AddIngredient(ItemID.GoldOre);
+            recipe.SetResult(this);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+			
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.SandBlock,1);
+			recipe.AddIngredient(ItemID.PlatinumOre);
+            recipe.SetResult(this);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Plastic.cs
@@ -0,0 +1,26 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Items 
+{
+    public class Plastic : ModItem 
+    {
+		public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 24;
+            item.maxStack = 99;
+		}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Gel,5);
+			recipe.AddIngredient(ItemID.AshBlock,5);
+            recipe.SetResult(this);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+        }
+    }
+}
