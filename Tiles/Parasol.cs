7869db7e970ca1ed2f8a3318973af62830936908
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 02:29:11 +1000

    fixed parasol

--- Tiles/Parasol.cs
+++ Tiles/Parasol.cs
@@ -3,6 +3,8 @@ using Terraria.ModLoader;
 using Terraria.ObjectData;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
+using Terraria.Enums;
 using static Terraria.ModLoader.ModContent;
 
 namespace Terrarchitect.Tiles
@@ -15,6 +17,7 @@ namespace Terrarchitect.Tiles
             Main.tileNoAttach[Type] = true;
             Main.tileLavaDeath[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
             TileObjectData.newTile.Width = 1;
             TileObjectData.newTile.Height = 3;
             TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 18 };
@@ -29,18 +32,12 @@ namespace Terrarchitect.Tiles
 
         public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
         {
-            Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
-
-            if (Main.drawToScreen)
-            {
-                zero = Vector2.Zero;
-            }
-
+            Vector2 zero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange, Main.offScreenRange);
             Tile tile = Main.tile[i, j];
 
             if (tile.frameY == 0)
             {
-                spriteBatch.Draw(mod.GetTexture("Tiles/ParasolTop"), new Vector2((i * 16) - (int)Main.screenPosition.X - 15, (j * 16) - (int)Main.screenPosition.Y - 24) + zero, new Rectangle(0, 0, 62, 24), Color.White);
+                spriteBatch.Draw(mod.GetTexture("Tiles/ParasolTop"), new Vector2((i * 16) - (int)Main.screenPosition.X - 22, (j * 16) - (int)Main.screenPosition.Y - 24) + zero, new Rectangle(0, 0, 62, 24), Color.White);
             }
             
             return true;
