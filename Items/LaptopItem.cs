da0cc46750068cb504eed2829282423702af13de
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-11-26 21:54:52 +0000

    Add files via upload

---
+++ Items/LaptopItem.cs
@@ -0,0 +1,37 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Items
+{
+    public class LaptopItem : ModItem
+    {
+        public override void SetStaticDefaults(){DisplayName.SetDefault("Laptop");}
+
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 24;
+            item.maxStack = 99;
+            item.value = 5;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.Laptop>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wire,2);
+			recipe.AddIngredient(mod, "Plastic",2);
+			recipe.AddIngredient(mod, "ElectronicComponent",2);
+            recipe.SetResult(this);
+            recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
+            recipe.AddRecipe();
+        }
+    }
+}
