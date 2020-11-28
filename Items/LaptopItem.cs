3a5403d1ed54dd74d1ad2193ed1f6ecc0b521e9e
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-11-28 22:45:41 +0100

    Update LaptopItem.cs

--- Items/LaptopItem.cs
+++ Items/LaptopItem.cs
@@ -27,7 +27,7 @@ namespace Terrarchitect.Items
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.Wire,2);
-			recipe.AddIngredient(mod, "Plastic",2);
+			recipe.AddIngredient(mod, "PlasticBar",2);
 			recipe.AddIngredient(mod, "ElectronicComponent",2);
             recipe.SetResult(this);
             recipe.AddTile(TileType<Tiles.TerrarchitectCatalogue>());
