98e7ce910b2d88d5a6d1fa0abdfa78516d50fb7a
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-12-05 18:37:27 +0000

    ignore the badly made placeholder sprite

---
+++ Tiles/WallMountedPhone.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+using Terraria.DataStructures;
+
+namespace Terrarchitect.Tiles
+{
+    public class WallMountedPhone : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16 };
+			TileObjectData.newTile.Origin = new Point16(0, 0);
+            TileObjectData.addTile(Type);
+            disableSmartCursor = true;
+        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 22, 22, ItemType<Items.WallMountedPhoneItem>());
+        }
+    }
+}
