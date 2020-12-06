91e0932065fc607551d0a5430035612cd79ea2ea
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-12-06 20:50:19 +0000

    Add files via upload

---
+++ Tiles/PinballMachine.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.Enums;
+
+namespace Terrarchitect.Tiles
+{
+	public class PinballMachine : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 18, 16 };
+            TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceLeft;
+            TileObjectData.addAlternate(1);
+            TileObjectData.addTile(Type);
+            disableSmartCursor = true;
+			animationFrameHeight = 56;
+		}
+		public override void AnimateTile(ref int frame, ref int frameCounter) {
+			if (++frameCounter >= 9)
+			{
+				frameCounter = 0;
+				frame = ++frame % 4;
+			}
+		}
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+			Item.NewItem(i * 16, j * 16, 16, 32, ModContent.ItemType<Items.PinballMachineItem>());
+		}
+	}
+}
