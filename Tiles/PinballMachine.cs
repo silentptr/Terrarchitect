73aa37403261a83d3b42ce9d0787ac7c88d7645b
GurgleNurgle <74212862+GurgleNurgle@users.noreply.github.com>
2020-12-09 10:13:43 +0100

    Update PinballMachine.cs

--- Tiles/PinballMachine.cs
+++ Tiles/PinballMachine.cs
@@ -26,7 +26,7 @@ namespace Terrarchitect.Tiles
 			animationFrameHeight = 56;
 		}
 		public override void AnimateTile(ref int frame, ref int frameCounter) {
-			if (++frameCounter >= 9)
+			if (++frameCounter >= 8)
 			{
 				frameCounter = 0;
 				frame = ++frame % 4;
