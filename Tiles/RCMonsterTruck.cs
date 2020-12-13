18319b6a9a29e05c9342bd7905d926b126d65792
Zandberg-x <68066390+Zandberg-x@users.noreply.github.com>
2020-12-13 22:23:17 +0000

    Add files via upload

---
+++ Tiles/RCMonsterTruck.cs
@@ -0,0 +1,32 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using static Terraria.ModLoader.ModContent;
+
+namespace Terrarchitect.Tiles
+{
+    public class RCMonsterTruck : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16 };
+            TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceLeft;
+            TileObjectData.addAlternate(1);
+            TileObjectData.addTile(Type);
+            disableSmartCursor = true;
+         }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 24, 24, ItemType<Items.RCMonsterTruckItem>());
+        }
+    }
+}
